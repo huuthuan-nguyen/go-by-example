b57350a2f89bb974474371a56ee4bcde00556de1
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2020-06-12 17:47:23 +0700

    Formatting the text with string formatting functions

---
+++ string_formatting.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type point struct {
+	x, y int
+}
+
+func main() {
+	p := point{1, 2}
+	fmt.Printf("%v\n", p)
+
+	fmt.Printf("%+v\n", p)
+	fmt.Printf("%#v\n", p)
+	fmt.Printf("%T\n", p)
+	fmt.Printf("%t\n", p)
+	fmt.Printf("%d\n", p)
+}
