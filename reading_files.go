a1c80033afd6c324e34e059186d10e7ee974c1b3
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2020-06-17 09:23:29 +0700

    Reading and Writing files

--- reading_files.go
+++ reading_files.go
@@ -15,11 +15,11 @@ func check(e error) {
 }
 
 func main() {
-	dat, err := ioutil.ReadFile("/tmp/dat")
+	dat, err := ioutil.ReadFile("./dat")
 	check(err)
 	fmt.Print(string(dat))
 
-	f, err := os.Open("/tmp/dat")
+	f, err := os.Open("./dat")
 	check(err)
 
 	b1 := make([]byte, 5)
